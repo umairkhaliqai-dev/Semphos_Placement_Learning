88a068cd591dbfcfb1a001b1da33e8d3197cb0b9
Umair Khaliq <umairkhaliq.ai@gmail.com>
2026-03-29 13:25:02 +0100

    Day 1: Python course started with hello world

---
+++ code/day01/helloworld.py
@@ -0,0 +1,5 @@
+print("Hello, Pyhton Learner!")
+print("Lets start 30 Dyas of Pyhton Journey")
+# Day 1: My First Python Program
+print("Hello, Python Learner!")
+print("Let's start 30 Days of Python Journey")
